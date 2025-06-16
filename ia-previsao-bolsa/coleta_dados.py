05d1f6eb34042d5f105d293cb68d1f1c2167fdab
Luiz Felipe Fonseca <luizfelipefonsecaa16@gmail.com>
2025-06-16 11:22:35 -0300

    Update coleta_dados.py

--- ia-previsao-bolsa/coleta_dados.py
+++ ia-previsao-bolsa/coleta_dados.py
@@ -1,1 +1,7 @@
+```python
+import yfinance as yf
 
+def baixar_dados(ticker="PETR4.SA", periodo="30d", intervalo="1m"):
+    data = yf.download(ticker, period=periodo, interval=intervalo)
+    return data
+```
