a1c4221b1036ec7afcb162bacf9687efe5676783
Luiz Felipe Fonseca <luizfelipefonsecaa16@gmail.com>
2025-06-16 21:20:21 -0300

    Atualizar o preprocessamento.py

--- ia-previsao-bolsa/preprocessamento.py
+++ ia-previsao-bolsa/preprocessamento.py
@@ -1,4 +1,3 @@
-```python
 import numpy as np
 import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
@@ -19,4 +18,3 @@ def preparar_dados(df: pd.DataFrame, features, janela=3):
         y_seq.append(y[i+janela])
 
     return np.array(X_seq), np.array(y_seq), scaler
-```
